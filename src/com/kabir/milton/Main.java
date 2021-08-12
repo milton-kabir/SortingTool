45c80e7d90aea94b687a11ec10b33c4bb484eafd
milton-kabir <milton.cse.pust@gmail.com>
2021-08-13 04:27:58 +0600

    First Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -1,8 +1,29 @@
+//package sorting;
 package com.kabir.milton;
 
-public class Main {
+import java.util.*;
 
-    public static void main(String[] args) {
-        // write your code here
+public class Main {
+    public static void main(final String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Long> ar = new ArrayList<>();
+        while (scanner.hasNextLong()) {
+            long number = scanner.nextLong();
+            // write your code here
+            ar.add(number);
+        }
+        Collections.sort(ar);
+        int sz = ar.size();
+        System.out.println("Total numbers: " + sz + ".");
+        Long mx = ar.get(sz - 1);
+        int cnt = 0;
+        for (int i = sz - 1; i >= 0; i--) {
+            if (ar.get(i) < mx) {
+                break;
+            } else {
+                cnt++;
+            }
+        }
+        System.out.println("The greatest number: " + mx + " (" + cnt + " time(s)).");
     }
 }

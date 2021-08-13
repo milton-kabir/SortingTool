0cc8eb89315de54b87025bc79495d181b20bfcd4
milton-kabir <milton.cse.pust@gmail.com>
2021-08-13 08:00:58 +0600

    Fourth Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -27,68 +27,79 @@ public class Main {
 
     public static void main(final String[] args) {
         String st = "word";
+        String tt = "natural";
         int ck = 0;
         for (int i = 0; i < args.length; i++) {
             if (args[i].equals("-dataType")) {
                 st = args[i + 1];
             }
-            if (args[i].equals("-sortIntegers")) {
-                ck = 1;
+            if (args[i].equals("-sortingType")) {
+                tt = args[i + 1];
             }
         }
-        if (ck == 1) {
-            Scanner scanner = new Scanner(System.in);
-            ArrayList<Long> ar = new ArrayList<>();
-            while (scanner.hasNextLong()) {
-                long number = scanner.nextLong();
-                // write your code here
-                ar.add(number);
-            }
-            Collections.sort(ar);
-            int sz = ar.size();
-            System.out.println("Total numbers: " + sz + ".");
-            System.out.print("Sorted data:");
-            for (int i = 0; i < ar.size(); i++) {
-                System.out.print(" " + ar.get(i));
-            }
-
-            return;
-        }
-
         if (st.equals("long")) {
             Scanner scanner = new Scanner(System.in);
-            ArrayList<Long> ar = new ArrayList<>();
+            Set<Long> ar = new HashSet<>();
+            HashMap<String, Integer> hm = new HashMap<>();
+            int sz = 0;
             while (scanner.hasNextLong()) {
                 long number = scanner.nextLong();
+                sz++;
                 // write your code here
                 ar.add(number);
-            }
-            Collections.sort(ar);
-            int sz = ar.size();
-            System.out.println("Total numbers: " + sz + ".");
-            Long mx = ar.get(sz - 1);
-            int cnt = 0;
-            for (int i = sz - 1; i >= 0; i--) {
-                if (ar.get(i) < mx) {
-                    break;
+                if (hm.containsKey(Long.toString(number))) {
+                    int xx = hm.get(Long.toString(number));
+                    xx++;
+                    hm.put(Long.toString(number), xx);
                 } else {
-                    cnt++;
+                    hm.put(Long.toString(number), 1);
                 }
             }
-            int pp = 100 * cnt / sz;
+            List<Long> list = new ArrayList<Long>(ar);
+            Collections.sort(list);
 
-            System.out.println("The greatest number: " + mx + " (" + cnt + " time(s), " + pp + "%).");
+            System.out.println("Total numbers: " + sz + ".");
+            if (tt.equals("natural")) {
+                System.out.print("Sorted data:");
+                for (Long ii : list) {
+                    for (int j = 0; j < hm.get(Long.toString(ii)); j++)
+                        System.out.print(" " + ii);
+                }
 
+            } else {
+
+                Map<String, Integer> hm1 = sortByValue(hm);
+                for (String ii : hm1.keySet()) {
+                    int pq = 100 * hm1.get(ii) / sz;
+                    String ts = ii;
+                    int xx = hm1.get(ii);
+                    List<Long> mm = new ArrayList<>();
+                    for (int jj = 0; jj < list.size(); jj++) {
+                        String sss = Long.toString(list.get(jj));
+                        if (hm.get(sss) == xx) {
+                            mm.add(list.get(jj));
+                        }
+                    }
+                    for (int i = 0; i < mm.size(); i++) {
+                        System.out.println(mm.get(i) + ": " + xx + " time(s), " + pq + "%");
+                        list.remove(mm.get(i));
+                    }
+
+                }
+
+            }
         } else if (st.equals("word")) {
             Scanner scanner = new Scanner(System.in);
             HashMap<String, Integer> hm = new HashMap<>();
             HashMap<String, Integer> myMap = new HashMap<>();
+            ArrayList<String> ss = new ArrayList<>();
             int sz = 0;
             while (scanner.hasNextLine()) {
                 String[] number = scanner.nextLine().split("\\s+");
                 // write your code here
                 sz += number.length;
                 for (int i = 0; i < number.length; i++) {
+                    ss.add(number[i]);
                     if (myMap.containsKey(number[i])) {
                         int xx = myMap.get(number[i]);
                         xx++;
@@ -96,27 +107,52 @@ public class Main {
                     } else {
                         myMap.put(number[i], 1);
                     }
-                    hm.put(number[i], number[i].length());
                 }
             }
-            Map<String, Integer> hm1 = sortByValue(hm);
+            Map<String, Integer> hm1 = sortByValue(myMap);
+            Collections.sort(ss);
+            Set<String> ar = new HashSet<>();
+            ar.addAll(ss);
+
             System.out.println("Total words: " + sz + ".");
-            String mx = "";
-            for (Map.Entry<String, Integer> en : hm1.entrySet()) {
-                mx = en.getKey();
+            if (tt.equals("natural")) {
+                System.out.print("Sorted data:");
+                for (int i = 0; i < ss.size(); i++) {
+                    System.out.print(" " + ss.get(i));
+                }
+
+            } else {
+                for (String ii : hm1.keySet()) {
+                    int pq = 100 * hm1.get(ii) / sz;
+                    String ts = ii;
+                    int xx = hm1.get(ii);
+                    List<String> mm = new ArrayList<>();
+                    for (String jj : ar) {
+                        String sss = jj;
+                        //System.out.println(sss);
+                        if (hm1.get(sss) == xx) {
+                            mm.add(jj);
+                        }
+                    }
+                    Collections.sort(mm);
+                    for (int i = 0; i < mm.size(); i++) {
+                        System.out.println(mm.get(i) + ": " + xx + " time(s), " + pq + "%");
+                        ar.remove(mm.get(i));
+                    }
+
+                }
+
             }
-            int cnt = myMap.get(mx);
-            int pp = 100 * cnt / sz;
-            System.out.println("The longest word: " + mx + " (" + cnt + " time(s), " + pp + "%).");
 
         } else {
             Scanner scanner = new Scanner(System.in);
-            HashMap<String, Integer> hm = new HashMap<>();
             HashMap<String, Integer> myMap = new HashMap<>();
+            ArrayList<String> ss = new ArrayList<>();
             int sz = 0;
             while (scanner.hasNextLine()) {
                 String number = scanner.nextLine();
                 // write your code here
+                ss.add(number);
                 if (myMap.containsKey(number)) {
                     int xx = myMap.get(number);
                     xx++;
@@ -124,21 +160,45 @@ public class Main {
                 } else {
                     myMap.put(number, 1);
                 }
-                hm.put(number, number.length());
                 sz++;
             }
-            Map<String, Integer> hm1 = sortByValue(hm);
+            Map<String, Integer> hm1 = sortByValue(myMap);
+            Collections.sort(ss);
+            Set<String> ar = new HashSet<>();
+            ar.addAll(ss);
             System.out.println("Total words: " + sz + ".");
-            String mx = "";
-            for (Map.Entry<String, Integer> en : hm1.entrySet()) {
-                mx = en.getKey();
-            }
-            int cnt = myMap.get(mx);
-            int pp = 100 * cnt / sz;
-            System.out.println("The longest line:\n" +
-                    mx + "\n" +
-                    "(" + cnt + " time(s), " + pp + "%).");
-        }
+            if (tt.equals("natural")) {
 
+                System.out.print("Sorted data:");
+                for (int i = 0; i < ss.size(); i++) {
+                    System.out.print(" " + ss.get(i));
+                }
+
+            } else {
+
+                for (String ii : hm1.keySet()) {
+                    int pq = 100 * hm1.get(ii) / sz;
+                    String ts = ii;
+                    int xx = hm1.get(ii);
+                    List<String> mm = new ArrayList<>();
+                    for (String jj : ar) {
+                        String sss = jj;
+                        //System.out.println(sss);
+                        if (hm1.get(sss) == xx) {
+                            mm.add(jj);
+                        }
+                    }
+                    Collections.sort(mm);
+                    for (int i = 0; i < mm.size(); i++) {
+                        System.out.println(mm.get(i) + ": " + xx + " time(s), " + pq + "%");
+                        ar.remove(mm.get(i));
+                    }
+
+                }
+
+
+            }
+
+        }
     }
 }
